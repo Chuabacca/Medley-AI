ac6cac99ce34570f72c47ec9c2c88a89dcf3f587
Chuabacca <jchua.yf@gmail.com>
2025-10-24 15:03:35 -0700

    Initial Commit

---
+++ Medley/MedleyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedleyApp.swift
+//  Medley
+//
+//  Created by Jonathan Chua on 10/24/25.
+//
+
+import SwiftUI
+
+@main
+struct MedleyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
